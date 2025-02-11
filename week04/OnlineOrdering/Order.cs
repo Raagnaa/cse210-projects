0beec73b2cdf35ce9e9f14f256b8451f71652cc2
Emmanuel <94509296+Raagnaa@users.noreply.github.com>
2025-02-11 00:27:29 -0600

    Online

---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Text;
+
+public class Order
+{
+    private List<Product> products;
+
+    private Customer customer;
+
+    public Order(Customer customer)
+    {
+        this.customer = customer;
+        products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public double CalculateTotalCost()
+    {
+        double totalCost = 0;
+        foreach (var product in products)
+        {
+            totalCost += product.TotalCost();
+        }
+        totalCost += customer.IsInUSA() ? 5 : 35;
+        return totalCost;
+    }
+
+    public string GetPackingLabel()
+    {   
+        StringBuilder packingLabel = new StringBuilder();
+        foreach (var product in products)
+        {
+            packingLabel.AppendLine($"{product.GetName()} (ID: {product.GetProductId()})");
+        }
+        return packingLabel.ToString();
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"{customer.GetName()}\n{customer.GetAddress()}";
+    }
+
+}
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("2345 Fresnos Ave", "Brownsville", "TX", "USA");
+        Customer customer1 = new Customer("Emmanuel Garcia", address1);
+
+        Product product1 = new Product("Mouse Pad", "MOUSEP", 40, 1);
+        Product product2 = new Product("Desk", "DESK", 240, 1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Console.WriteLine("Order 1:");
+        Console.WriteLine("Packing Label:");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.CalculateTotalCost()}");
+
+        Console.WriteLine();
+
+        Address address2 = new Address("3 Fransisco Rincon", "Matamoros", "Tamaulipas", "Mexico");
+        Customer customer2 = new Customer("Fernando Garcia", address1);
+
+        Product product3 = new Product("Boat", "BOAT", 42220, 1);
+        Product product4 = new Product("Sword", "SWORD", 280, 1);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product3);
+        order2.AddProduct(product4);
+
+        Console.WriteLine("Order 2:");
+        Console.WriteLine("Packing Label:");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.CalculateTotalCost()}");
     }
 }
