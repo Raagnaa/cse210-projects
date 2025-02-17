7825452b7002a798f587fe73170c178af033782d
Emmanuel <94509296+Raagnaa@users.noreply.github.com>
2025-02-17 13:55:51 -0600

    Update Program.cs

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,16 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Test Assignment
         Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
         Console.WriteLine(assignment.GetSummary());
 
-        // Test MathAssignment
         MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "8-19");
         Console.WriteLine(mathAssignment.GetSummary());
         Console.WriteLine(mathAssignment.GetHomeworkList());
 
-        // Test WritingAssignment
         WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
         Console.WriteLine(writingAssignment.GetSummary());
         Console.WriteLine(writingAssignment.GetWritingInformation());
