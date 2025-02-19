e4d16d75a163206337331450b56a44bc4b2aec8f
Emmanuel <94509296+Raagnaa@users.noreply.github.com>
2025-02-18 21:12:44 -0600

    wk6-1

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4),
+            new Rectangle("Blue", 5, 3),
+            new Circle("Green", 2.5)
+        };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.Color}, Area: {shape.GetArea()}");
+        }
     }
 }
