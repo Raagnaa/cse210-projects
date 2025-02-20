abe521a5bfd3c17c68855ffd5ea880f8f2e2319c
Emmanuel <94509296+Raagnaa@users.noreply.github.com>
2025-02-19 20:27:56 -0600

    comment bonus points

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,3 +1,4 @@
+//I make a Due Date to the goals beacuse i learn in the mission that a goal without a Date is not going to happen, first i thought in bonus poitns for the goals, but it didn't turn out very well for me, and neither did the due date thing, but it's what can be done and work.
 using System;
 public class Program
 {
