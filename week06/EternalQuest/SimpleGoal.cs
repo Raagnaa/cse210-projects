9c981ed3a3d1b56eeba6c2b196acbf6abd334b6d
Emmanuel <94509296+Raagnaa@users.noreply.github.com>
2025-02-19 20:29:25 -0600

    Update SimpleGoal.cs

--- week06/EternalQuest/SimpleGoal.cs
+++ week06/EternalQuest/SimpleGoal.cs
@@ -2,8 +2,7 @@ using System;
 
 public class SimpleGoal : Goal
 {
-    public SimpleGoal(string shortName, string description, int points, DateTime dueDate)
-        : base(shortName, description, points, dueDate) { }
+    public SimpleGoal(string shortName, string description, int points, DateTime dueDate) : base(shortName, description, points, dueDate) { }
 
     public override string GetDetailsString() => $"Simple Goal: {ShortName}, {Description}, {Points} points, Due Date: {DueDate.ToShortDateString()}";
     public override string GetStringRepresentation() => $"SimpleGoal: {ShortName}";
